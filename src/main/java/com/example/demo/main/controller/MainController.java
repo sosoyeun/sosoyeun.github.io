44def72fffe01dcc477eba585710e1ef6025a33a
soyeun <soso1yun@daum.net>
2020-01-25 00:07:38 +0900

    index push

--- src/main/java/com/example/demo/main/controller/MainController.java
+++ src/main/java/com/example/demo/main/controller/MainController.java
@@ -7,13 +7,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
-    @RequestMapping(value = "test", method = RequestMethod.GET)
+    @RequestMapping(value = "index", method = RequestMethod.GET)
     public String index(){
-        return "/view/index";
+        return "/view/layout/index";
     }
 
-    @RequestMapping(value = "test", method = RequestMethod.GET)
+    @RequestMapping(value = "header", method = RequestMethod.GET)
+    public String header(){
+        return "/view/fragments/header";
+    }
+
+    @RequestMapping(value = "main", method = RequestMethod.GET)
     public String main(){
-        return "/view/test";
+        return "/view/main";
+    }
+
+    @RequestMapping(value = "footer", method = RequestMethod.GET)
+    public String footer(){
+        return "/view/fragments/footer";
     }
 }
